a47b7b111e0a229a9ccf843036f6862527214c50
Putradindra <96910925+Putradindra@users.noreply.github.com>
2022-04-16 09:04:57 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -3,7 +3,7 @@
 let handler = async m => m.reply(`
 ╭═════════════════
 ║   𝐆𝐑𝐎𝐔𝐏 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋
-║https://chat.whatsapp.com/Jzd9DEVBODtNBk1VCNrV
+║Aku suka Body Mamah Muda Awokawokawok (Swister Gans)
 ╰══════════════════
 
 `.trim()) 
