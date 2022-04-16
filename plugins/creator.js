86284c5d4c38119d6902eb9141929a5b3adaee2b
Putradindra <96910925+Putradindra@users.noreply.github.com>
2022-04-16 09:01:08 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -14,7 +14,7 @@ const q = {
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6288217843188', 'Reza Temayang', m)
+conn.sendContact(m.chat, '6289504575881', 'Owner Null Bot', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
